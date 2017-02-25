0985a0c0c0ae8705c337f3d6953e778650aac8ac
dvdlopez4 <dvdlopez4@gmail.com>
2017-02-25 12:14:16 -0800

    Testing SDL_ttf

--- Win32Project1/Win32Project1/main.cpp
+++ Win32Project1/Win32Project1/main.cpp
@@ -1,4 +1,5 @@
 #include <SDL.h>
+#include <SDL_ttf.h>
 #include <iostream>
 
 int main(int argc, char* argv[])
@@ -15,7 +16,7 @@ int main(int argc, char* argv[])
 	{
 		/* Sets window with the parameters */
 		window = SDL_CreateWindow("Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
-
+		
 		if (window == nullptr)
 			std::cout << "Window creation error: " << SDL_GetError() << std::endl;
 		else
@@ -23,8 +24,30 @@ int main(int argc, char* argv[])
 			renderTarget = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 			// Window Created
 			SDL_SetRenderDrawColor(renderTarget, 0, 0, 0x55, 0x0);
+			
+			SDL_Color color;
+			TTF_Font *font = TTF_OpenFont("SEASRN__.ttf", 20);
+			SDL_Rect textRect;
+			SDL_Surface *textSurface = nullptr;
+			SDL_Texture *textTexture = nullptr;
+
+			color = { 255, 255, 255 };
+			textRect.x = (int)640 / 2;
+			textRect.y = (int)480 / 2;
+
+			textSurface = TTF_RenderText_Solid(font, "Karen Yulissa Beza Lopez", color);
+			textTexture = SDL_CreateTextureFromSurface(renderTarget, textSurface);
+
+			if (textTexture == nullptr)
+				printf("Unable to render text surface! SDL_ttf Error: %s\n", TTF_GetError());
+
+			SDL_QueryTexture(textTexture, nullptr, nullptr, &textRect.w, &textRect.h);
+			SDL_FreeSurface(textSurface);
+			textSurface = nullptr;
+
 
 			SDL_RenderClear(renderTarget);
+			SDL_RenderCopy(renderTarget, textTexture, nullptr, &textRect);
 			SDL_RenderPresent(renderTarget);
 			SDL_Delay(5000); // The window will display for 5 seconds
 
